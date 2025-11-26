d916cdc48abb7d86b87297b046c7c6bcbb3a353b
Mateo Chavez <mchavez.castillo23@gmail.com>
2025-11-26 17:05:55 -0500

    feat: creat servicio de individuo e implementar logica

--- Refugiados-App/src/main/java/services/impl/IndividuoServiceImpl.java
+++ Refugiados-App/src/main/java/services/impl/IndividuoServiceImpl.java
@@ -1,7 +1,86 @@
 package services.impl;
 
+import lombok.AllArgsConstructor;
+import model.Individuo;
+import model.enums.TipoDocumento;
+import repository.IndividuoRepo;
 import services.IndividuoService;
 
+import java.sql.SQLException;
+import java.util.Optional;
+
+@AllArgsConstructor
 public class IndividuoServiceImpl implements IndividuoService {
 
+    private final IndividuoRepo individuoRepo;
+
+    @Override
+    public Individuo crearIndividuo(Individuo individuo) throws SQLException {
+        if(verificarNoVacios(individuo)){
+            throw new IllegalArgumentException("Campos del individuo no deben ser vacios");
+        }
+        return individuoRepo.guardarIndividuo(individuo);
+    }
+
+    @Override
+    public Optional<Individuo> buscarIndividuoPorTipoDocumentoYNumero(TipoDocumento tipoDocumento, String numeroDocumento) throws SQLException {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Individuo> buscarIndividuoPorNumeroDocumento(String numeroDocumento) throws SQLException {
+        return Optional.empty();
+    }
+
+    private boolean verificarNoVacios(Individuo individuo) {
+        if(individuo.getNombre().isBlank()){
+            return false;
+        }
+        if(individuo.getApellido().isBlank()){
+            return false;
+        }
+        if(individuo.getGenero().isBlank()){
+            return false;
+        }
+        if(individuo.getFechaNacimiento() == null){
+            return false;
+        }
+        if(individuo.getPais_origen().isBlank()){
+            return false;
+        }
+        if(individuo.getIdiomaPrincipal().isBlank()){
+            return false;
+        }
+        if (individuo.getNivelEducacion() == null) {
+            return false;
+        }
+        if(individuo.getTelefono().isBlank()){
+            return false;
+        }
+        if (individuo.getEstatusLegal() == null) {
+            return false;
+        }
+        if (individuo.getTipoDocumento() == null) {
+            return false;
+        }
+        if (individuo.getNumeroDocumento().isBlank()) {
+            return false;
+        }
+        if (individuo.getDiscapacidad().isBlank()) {
+            return false;
+        }
+        if (individuo.getEnfermedadCronica().isBlank()) {
+            return false;
+        }
+        if (individuo.getEmbarazada() == null) {
+            return false;
+        }
+        if (individuo.getEstadoEmpleo() == null) {
+            return false;
+        }
+        if (individuo.getRepresentanteHogar() == null){
+            return false;
+        }
+        return true;
+    }
 }
