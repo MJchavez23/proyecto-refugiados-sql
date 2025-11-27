bb7ee3e62106278e8bee3864c8f3fa252403e8b4
JGabela <87086339+JGabela@users.noreply.github.com>
2025-11-26 20:32:56 -0500

    Update CRUD Historial.sql
    Creacion del trigger

--- SQL Queries/CRUD Historial.sql
+++ SQL Queries/CRUD Historial.sql
@@ -1,12 +1,14 @@
 --No se deben eliminar datos del historial medico
 
+--Crear el historial 
 CREATE OR REPLACE FUNCTION crear_historial(
-a_idindividuo INTEGER, 
+a_idservicio INTEGER,
+a_idindividuo INTEGER,
 a_idpersonal INTEGER,
-a_fecha_servicio DATE, 
+a_fecha_servicio DATE,
 a_resultado VARCHAR(120),
 a_seguimiento BOOL,
-a_fecha_seguimiento DATE
+fecha_seguimiento BOOL
 )
 RETURNS void AS 
 $$
@@ -29,4 +31,39 @@ a_fecha_seguimiento
 END;
 $$ LANGUAGE plpgsql;
 
+--Leer el historial
+CREATE OR REPLACE FUNCTION leer_Historial(a_idhistorial INTEGER)
+RETURNS SETOF HistorialServicio AS 
+$$
+BEGIN
+SELECT * FROM HistorialServicio WHERE idhistorial = a_idhistorial;
+END;
+$$ LANGUAGE plpgsql;
 
+--trigger para historial
+CREATE OR REPLACE FUNCTION actualizarBitacora_historial()
+RETURNS TRIGGER AS
+$$
+DECLARE 
+BEGIN
+INSERT INTO Bitacora (
+idbitacora,
+accion,
+tabla,
+fecha,
+usuario
+) 
+VALUES(
+nextval(),
+TG_OP,
+'HistorialServicio',
+CURRENT_DATE,
+CURRENT_USER
+);
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_historial
+AFTER INSERT OR UPDATE OR DELETE ON Historial
+FOR EACH ROW
+EXECUTE FUNCTION actualizarBitacora_historial();
