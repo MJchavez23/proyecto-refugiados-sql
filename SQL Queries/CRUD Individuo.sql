67a13d247d3064ba5c0d63c450ac6995ec33ae03
JGabela <87086339+JGabela@users.noreply.github.com>
2025-11-26 20:53:14 -0500

    Update CRUD Individuo.sql
    Creacion del trigger para individuo

--- SQL Queries/CRUD Individuo.sql
+++ SQL Queries/CRUD Individuo.sql
@@ -66,4 +66,33 @@ $$
 BEGIN
 DELETE FROM Individuo WHERE idindividuo = a_idindividuo;
 END;
+
 $$ LANGUAGE plpgsql;
+
+--trigger para individuo
+CREATE OR REPLACE FUNCTION actualizarBitacora_individuo()
+RETURNS TRIGGER AS
+$$
+DECLARE 
+BEGIN
+INSERT INTO Bitacora (
+idbitacora,
+accion,
+tabla,
+fecha,
+usuario
+) 
+VALUES(
+nextval(),
+TG_OP,
+'Individuo',
+CURRENT_DATE,
+CURRENT_USER
+);
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_individuo
+AFTER INSERT OR UPDATE OR DELETE ON Individuo
+FOR EACH ROW
+EXECUTE FUNCTION actualizarBitacora_individuo();
