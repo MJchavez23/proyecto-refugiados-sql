35feea7cdb00f0deac8a81ff3da2518b4e208a83
JGabela <87086339+JGabela@users.noreply.github.com>
2025-12-01 13:25:48 -0500

    Update and rename creador de tablas.sql to Creador Tablas.sql

---
+++ SQL Queries/Creador Tablas.sql
@@ -0,0 +1,102 @@
+--Este Script es para crear las tablas
+
+CREATE TABLE Refugio (
+id_refugio SERIAL PRIMARY KEY,
+nombre_refugio VARCHAR(50) NOT NULL, 
+ciudad_refugio VARCHAR(20) NOT NULL,
+pais_refugio VARCHAR(20) NOT NULL,
+referencia_refugio VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Hogar (
+id_hogar SERIAL PRIMARY KEY,
+id_refugio INTEGER NOT NULL,
+nombre_hogar VARCHAR(20) NOT NULL,
+fecha_llegada_refugio DATE NOT NULL,
+FOREIGN KEY(id_refugio) REFERENCES Refugio(id_refugio)
+);
+
+CREATE TABLE Individuo (
+id_individuo SERIAL PRIMARY KEY,
+id_hogar INTEGER,
+nombre VARCHAR(10) NOT NULL,
+apellido VARCHAR(10) NOT NULL,
+genero VARCHAR(10) NOT NULL,
+fecha_nacimiento DATE NOT NULL,
+pais_origen VARCHAR(20) NOT NULL,
+idioma_principal VARCHAR(20) NOT NULL,
+nivel_educativo VARCHAR(20),
+telefono VARCHAR(10),
+estatus_legal VARCHAR(20),
+tipo_documento VARCHAR(20),
+numero_documento VARCHAR(20),
+discapacidad VARCHAR(20), 
+enfermedad_cronica VARCHAR(20),
+estado_empleo VARCHAR(10),
+embarazo VARCHAR(10),
+representante_hogar BOOL,
+FOREIGN KEY(id_hogar) REFERENCES Hogar(id_hogar)
+);
+
+CREATE TABLE Alojamiento (
+id_habitacion SERIAL PRIMARY KEY,
+id_refugio INTEGER NOT NULL,
+id_hogar INTEGER NOT NULL,
+fecha_ingreso DATE NOT NULL,
+fecha_salida DATE,
+FOREIGN KEY(id_refugio) REFERENCES Refugio(id_refugio),
+FOREIGN KEY (id_hogar) REFERENCES Hogar(id_hogar)
+);
+
+CREATE TABLE Servicios (
+id_servicio SERIAL PRIMARY KEY, 
+tipo_servicio VARCHAR(40) NOT NULL,
+area_servicio VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE Personal (
+id_personal SERIAL PRIMARY KEY,
+nombre VARCHAR(20) NOT NULL,
+apellido VARCHAR(20) NOT NULL,
+fecha_nacimiento DATE NOT NULL,
+genero VARCHAR(10) NOT NULL,
+id_refugio INTEGER NOT NULL,
+FOREIGN KEY(id_refugio) REFERENCES Refugio(id_refugio)
+);
+
+CREATE TABLE HistorialServicio (
+id_historial SERIAL PRIMARY KEY,
+id_servicio INTEGER NOT NULL,
+id_individuo INTEGER NOT NULL,
+id_personal INTEGER NOT NULL,
+fecha_servicio DATE NOT NULL, 
+estado_servicio VARCHAR(120) NOT NULL,
+descripcion VARCHAR(50) NOT NULL, 
+ultima_modificacion DATE,
+FOREIGN KEY (id_servicio) REFERENCES Servicios(id_servicio),
+FOREIGN KEY(id_individuo) REFERENCES Individuo(id_individuo),
+FOREIGN KEY (id_personal) REFERENCES Personal(id_personal)
+);
+
+CREATE TABLE SaludRegistro (
+id_registro SERIAL PRIMARY KEY,
+id_individuo INTEGER NOT NULL,
+fecha_registro DATE NOT NULL,
+descripcion VARCHAR(40) NOT NULL,
+FOREIGN KEY(id_individuo) REFERENCES Individuo(id_individuo)
+);
+
+CREATE TABLE Vacunas (
+id_vacuna SERIAL PRIMARY KEY,
+id_individuo INTEGER NOT NULL,
+tipo_vacuna VARCHAR(20) NOT NULL,
+FOREIGN KEY(id_individuo) REFERENCES Individuo(id_individuo)
+);
+
+create table Bitacora(
+id_bitacora SERIAL primary key,
+accion VARCHAR(50),
+tabla VARCHAR(50),	
+fecha TIMESTAMP,
+usuario VARCHAR(50)
+);
