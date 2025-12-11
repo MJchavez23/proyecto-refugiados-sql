401efd17aa2f022d13b8100a9a6ce637ca02ad6e
JGabela <87086339+JGabela@users.noreply.github.com>
2025-12-10 19:04:21 -0500

    Funcional
    El archivo contiene el CRUD y el trigger de Historial_Servicios. Hay que tener en cuenta que de esta tabla no hay que editar ni eliminar ninguna entrada, por ende no hay funcionon de actualizar ni de eliminar.

--- SQL Queries/Trigger Historial.sql
+++ SQL Queries/Trigger Historial.sql
@@ -1,3 +1,45 @@
+--No se deben eliminar datos del historial medico
+
+--Crear el historial 
+CREATE OR REPLACE FUNCTION crear_historial(
+a_id_servicio INTEGER,
+a_id_individuo INTEGER,
+a_id_personal INTEGER,
+a_fecha_servicio DATE,
+a_resultado VARCHAR(255),
+a_seguimiento BOOLEAN,
+fecha_seguimiento BOOLEAN
+)
+RETURNS void AS 
+$$
+BEGIN INSERT INTO Historial_Servicios (
+id_individuo, 
+id_personal,
+fecha_servicio, 
+resultado,
+seguimiento,
+fecha_seguimiento
+) 
+VALUES (
+a_idindividuo,
+a_idpersonal,
+a_fecha_servicio, 
+a_resultado,
+a_seguimiento,
+a_fecha_seguimiento
+);
+END;
+$$ LANGUAGE plpgsql;
+
+--Leer el historial
+CREATE OR REPLACE FUNCTION leer_Historial(a_id_historial INTEGER)
+RETURNS SETOF Historial_Servicios AS 
+$$
+BEGIN
+SELECT * FROM Historial_Servicios WHERE id_historial = a_id_historial;
+END;
+$$ LANGUAGE plpgsql;
+
 --trigger para historial
 CREATE OR REPLACE FUNCTION actualizarBitacora_historial()
 RETURNS TRIGGER AS
@@ -5,24 +47,23 @@ $$
 DECLARE 
 BEGIN
 INSERT INTO Bitacora (
-usuario
-fecha,
+idbitacora,
 accion,
-tabla
+tabla,
+fecha,
+usuario
 ) 
 VALUES(
-CURRENT_USER,
-CURRENT_DATE,
+nextval(),
 TG_OP,
-'HistorialServicio'
+'Historial_Servicios',
+CURRENT_DATE,
+CURRENT_USER
 );
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER trigger_historial
-AFTER INSERT OR UPDATE OR DELETE ON HistorialServicio
+AFTER INSERT OR UPDATE OR DELETE ON Historial_Servicios
 FOR EACH ROW
 EXECUTE FUNCTION actualizarBitacora_historial();
-
-
-
