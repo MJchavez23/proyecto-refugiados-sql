9bdbf303bc8a84a11073cbbce712588a78b9c84a
Mateo Chávez <mchavez.castillo23@gmail.com>
2025-12-16 18:29:41 -0500

    Refactor hogar trigger and logging function

--- SQL Queries/Trigger Hogar.sql
+++ SQL Queries/Trigger Hogar.sql
@@ -1,41 +1,3 @@
---crear hogar
-CREATE OR REPLACE FUNCTION crearHogar (
-a_hogar_tamano INTEGER,
-a_fecha_llegada_pais DATE ,
-a_fecha_llegada_refugio DATE
-) RETURNS void AS 
-$$
-BEGIN 
-INSERT INTO Hogar(
-hogar_tamano,
-fecha_llegada_pais,
-fecha_llegada_refugio
-) VALUES(
-a_idrefugio,
-a_hogar_tamano,
-a_fecha_llegada_pais,
-a_fecha_llegada_refugio
-);
-END;
-$$ language plpgsql;
-
---leer hogar
-CREATE OR REPLACE FUNCTION leer_Hogar(a_id_hogar INTEGER)
-RETURNS SETOF Hogar AS 
-$$
-BEGIN
-SELECT * FROM Hogar WHERE id_hogar = a_id_hogar;
-END;
-$$ LANGUAGE plpgsql;
-
---borrar hogar
-CREATE OR REPLACE FUNCTION borrar_Hogar(a_id_hogar INTEGER)
-RETURNS void AS 
-$$
-BEGIN
-DELETE FROM Hogar WHERE id_hogar = a_id_hogar;
-END;
-$$ LANGUAGE plpgsql;
 
 --trigger hogar
 CREATE OR REPLACE FUNCTION actualizarBitacora_hogar()
@@ -63,4 +25,5 @@ CREATE TRIGGER trigger_hogar
 AFTER INSERT OR UPDATE OR DELETE ON Hogar
 FOR EACH ROW
 EXECUTE FUNCTION actualizarBitacora_hogar();
+
 
